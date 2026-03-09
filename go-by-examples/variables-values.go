397f20068aa7ec7ee33ac6dbb0c465cc5fa4addd
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2026-03-09 14:00:25 +0530

    variables, const, values

---
+++ go-by-examples/variables-values.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const VALUE string = "constant"
+
+func main() {
+	fmt.Println(1.0 / 3.0)
+	fmt.Println((math.Pow(3, 2)))
+
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1,2
+	fmt.Println(b,c)
+
+	var d = true
+	fmt.Println(d)
+
+	e := "declare&initialize"
+	fmt.Println(e)
+
+	var f int
+	fmt.Println(f)
+	
+	var g bool
+	fmt.Println(g)
+	
+	var h any
+	fmt.Println(h)
+
+	h = "nil to string"
+
+	fmt.Println(h)
+
+	const x int = 10
+	// x = 23 not allowed
+}
